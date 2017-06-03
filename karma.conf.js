b3ad4707760edc589306ac7901dd76c09c80bf45
eladh <hirsch.elad@gmail.com>
2017-06-03 17:36:20 +0300

    junit

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,75 @@
 module.exports = function (config) {
-  config.set({
-    // base path used to resolve all patterns
-    basePath: '',
+    config.set({
+        // base path used to resolve all patterns
+        basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'chai'],
 
-    // list of files/patterns to load in the browser
-    files: [{ pattern: 'spec.bundle.js', watched: false }],
+        // list of files/patterns to load in the browser
+        files: [{ pattern: 'spec.bundle.js', watched: false }],
 
-    // files to exclude
-    exclude: [],
+        // files to exclude
+        exclude: [],
 
-    plugins: [
-      require("karma-chai"),
-      require("karma-phantomjs-launcher"),
-      require("karma-mocha"),
-      require("karma-mocha-reporter"),
-      require("karma-sourcemap-loader"),
-      require("karma-webpack")
-    ],
+        plugins: [
+            require("karma-chai"),
+            require("karma-phantomjs-launcher"),
+            require("karma-mocha"),
+            require("karma-mocha-reporter"),
+            require("karma-junit-reporter"),
+            require("karma-sourcemap-loader"),
+            require("karma-webpack")
+        ],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 'spec.bundle.js': ['webpack', 'sourcemap'] },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: { 'spec.bundle.js': ['webpack', 'sourcemap'] },
 
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          { test: /\.js/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
-          { test: /\.html/, loader: 'raw' },
-          { test: /\.less/, loader: 'style!css!less' },
-          { test: /\.css$/, loader: 'style!css' }
-        ]
-      }
-    },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [
+                    { test: /\.js/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
+                    { test: /\.html/, loader: 'raw' },
+                    { test: /\.less/, loader: 'style!css!less' },
+                    { test: /\.css$/, loader: 'style!css' }
+                ]
+            }
+        },
 
-    webpackServer: {
-      noInfo: true // prevent console spamming when running in Karma!
-    },
+        webpackServer: {
+            noInfo: true // prevent console spamming when running in Karma!
+        },
 
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['junit'],
 
-    // web server port
-    port: 9876,
+        junitReporter : {
+            outputFile: 'test-results.xml',
+            useBrowserName: false,
+            outputDir: ''
+        },
 
-    // enable colors in the output
-    colors: true,
+        // web server port
+        port: 9876,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable colors in the output
+        colors: true,
 
-    // toggle whether to watch files and rerun tests upon incurring changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // toggle whether to watch files and rerun tests upon incurring changes
+        autoWatch: false,
 
-    // if true, Karma runs tests once and exits
-    singleRun: true
-  });
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+        // if true, Karma runs tests once and exits
+        singleRun: true
+    });
 };
